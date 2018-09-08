6224862b825ce69fcae92a7ddb794dfb2fba10cf
Andreu García <anduwet2@gmail.com>
2018-09-08 14:36:06 +0200

    Laravel 5.7 update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><i class="far fa-fw fa-registered"></i> {{ __('Register') }}</div>
+                <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">

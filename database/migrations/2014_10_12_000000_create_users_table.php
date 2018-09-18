8c519d2dfea3021f027e11a04b49fa560aea2fb0
Andreu García <anduwet2@gmail.com>
2018-09-18 21:08:24 +0200

    Avances

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->tinyInteger('random')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('api_token', 60)->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

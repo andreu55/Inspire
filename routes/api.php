585300e89b640cec76e0a45f01b17f9d38bd83ac
Andreu García <anduwet2@gmail.com>
2018-09-19 22:24:44 +0200

    routes

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('register', 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
+
 Route::post('ojo', function(Request $request) {
   return $request->all();
 });
